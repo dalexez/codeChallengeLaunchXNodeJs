23799b43c211bd8d9a3158372985413f97360031
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-30 17:40:25 -0500

    Agregando metodo filterByCertification en la clase StudentService, y su prueba unitaria en estado PASS

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -4,8 +4,8 @@ const Reader = require('./../../lib/utils/Reader')
 describe('Unit Tests for StudentService Class', () => {
   test('1) Test filterByCertification', () => {
     const students = Reader.readJsonFile('./test/data/students.json')
-    const studentsHaveCertification = StudentService.filterByCertification(students, true)
+    const studentsHaveCertification = StudentService.filterByCertification(students, false)
 
-    expect(studentsHaveCertification.length).toBe(18)
+    expect(studentsHaveCertification.length).toBe(14)
   })
 })
