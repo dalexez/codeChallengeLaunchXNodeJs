62d24374bf8a6945033c19984c69e59ff2a6be9a
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-30 16:43:53 -0500

    Agregando prueba unitaria para la clase Reader

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,10 @@
+const Reader = require('./../../lib/utils/Reader')
+
+describe('Unit Tests for Reader Class', () => {
+    test('1) Read the path of a file and the data on it', () => {
+      const students = Reader.readJsonFile('./students.json');
+  
+      expect(students.length).toBe(15);
+      expect(explorers[1].name).toBe('Woopa2');
+    });
+  });
