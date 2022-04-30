fdc375e76e329c28c80645a86a3211c733fc14a0
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-30 17:01:31 -0500

    Creacion de clase Reader, y su prueba en estado PASS

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -2,9 +2,9 @@ const Reader = require('./../../lib/utils/Reader')
 
 describe('Unit Tests for Reader Class', () => {
     test('1) Read the path of a file and the data on it', () => {
-      const students = Reader.readJsonFile('./students.json');
+      const students = Reader.readJsonFile('./test/data/students.json');
   
-      expect(students.length).toBe(15);
-      expect(students[1].name).toBe('');
+      expect(students.length).toBe(25);
+      expect(students[1].name).toBe('Lucinda');
     });
   });
