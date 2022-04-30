9823edd939c55da2fcbd824977bf3223de2c78c3
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-30 18:05:48 -0500

    Agregando metodo filterByCredits de la clase StudentService, y su prueba unitaria PASS

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -6,13 +6,13 @@ describe('Unit Tests for StudentService Class', () => {
     const students = Reader.readJsonFile('./test/data/students.json')
     const studentsHaveCertification = StudentService.filterByCertification(students, true)
 
-    expect(studentsHaveCertification.length).toBe(14)
+    expect(studentsHaveCertification.length).toBe(11)
   })
   test('2) Test filterByCredits', () => {
     const students = Reader.readJsonFile('./test/data/students.json')
     const studentsCredits = StudentService.filterByCredits(students, 500)
 
-    expect(studentsCredits.length).toBe(14)
-    expect(studentsCredits[4].name).toBe("Phillips")
+    expect(studentsCredits.length).toBe(15)
+    expect(studentsCredits[5].name).toBe("Kara")
   })
 })
