7cc44ec872af88c7fd1502f91ebdba3e9f858e3c
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-30 17:57:17 -0500

    Agregando prueba unitaria para metodo filterByCredits de la clase StudentService, modificacion en los test anteriores para verificar que GitHub Actions funcione

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -4,8 +4,15 @@ const Reader = require('./../../lib/utils/Reader')
 describe('Unit Tests for StudentService Class', () => {
   test('1) Test filterByCertification', () => {
     const students = Reader.readJsonFile('./test/data/students.json')
-    const studentsHaveCertification = StudentService.filterByCertification(students, false)
+    const studentsHaveCertification = StudentService.filterByCertification(students, true)
 
     expect(studentsHaveCertification.length).toBe(14)
+  })
+  test('2) Test filterByCredits', () => {
+    const students = Reader.readJsonFile('./test/data/students.json')
+    const studentsCredits = StudentService.filterByCredits(students, 500)
+
+    expect(studentsCredits.length).toBe(14)
+    expect(studentsCredits[4].name).toBe("Phillips")
   })
 })
