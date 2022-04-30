d20eadd5e8b900a1141c59b9971d44929cc505f1
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-30 18:35:27 -0500

    Agregando metodo allStudents en clase StudentService, metodo getAllStudents en clase StudentController

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -2,7 +2,7 @@ const StudentController = require('./../../lib/controllers/StudentController')
 
 describe('Unit Tests for StudentController Class', () => {
     test('1) Test for getAllStudents', () => {
-        const students = StudentController.getAllStudents()
+        const students = StudentController.getAllStudents('./test/data/students.json')
         
         expect(students.length).toBe(25);
     })
