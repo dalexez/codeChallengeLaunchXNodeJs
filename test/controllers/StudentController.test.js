0804ba5df4205272d0682618ff35768ed52baaad
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-30 19:01:34 -0500

    Agregando metodo getStudentsByCertification y su prueba en PASS, cambio en la manera de pasar el argumento path en los metodos

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -1,14 +1,15 @@
 const StudentController = require('./../../lib/controllers/StudentController')
+const path = "./test/data/students.json"
 
 describe('Unit Tests for StudentController Class', () => {
     test('1) Test for getAllStudents', () => {
-        const students = StudentController.getAllStudents('./test/data/students.json')
+        const students = StudentController.getAllStudents(path)
         
         expect(students.length).toBe(25);
     })
     test('2) Test for getStudentsByCertication', () => {
-        const students = StudentController.getStudentsByCertification(true)
+        const students = StudentController.getStudentsByCertification(true, path)
         
-        expect(students.length).toBe(10);
+        expect(students.length).toBe(11);
     })
 })
--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,15 +1,17 @@
 const StudentService = require('../../lib/services/StudentService')
 const Reader = require('./../../lib/utils/Reader')
 
+const path = "./test/data/students.json"
+
 describe('Unit Tests for StudentService Class', () => {
   test('1) Test filterByCertification', () => {
-    const students = Reader.readJsonFile('./test/data/students.json')
+    const students = Reader.readJsonFile(path)
     const studentsHaveCertification = StudentService.filterByCertification(students, true)
 
     expect(studentsHaveCertification.length).toBe(11)
   })
   test('2) Test filterByCredits', () => {
-    const students = Reader.readJsonFile('./test/data/students.json')
+    const students = Reader.readJsonFile(path)
     const studentsCredits = StudentService.filterByCredits(students, 500)
 
     expect(studentsCredits.length).toBe(15)
