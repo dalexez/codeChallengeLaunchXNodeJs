fecc023d460110793a278a8ffa2b252a99c05872
dalexez <dalexez@gmail.com>
2022-05-02 13:06:13 -0500

    Agregando endpoint para consultar email de estudiantes con Certificacion true, modificacion al metodo filterByCertification en clase StudentService

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -7,16 +7,17 @@ describe('Unit Tests for StudentController Class', () => {
     test('1) Test for getAllStudents', () => {
         const students = StudentController.getAllStudents(path)
         
-        expect(students.length).toBe(25);
+        expect(students.length).toBe(25)
     })
     test('2) Test for getStudentsByCertication', () => {
         const students = StudentController.getStudentsByCertification(boolean, path)
         
-        expect(students.length).toBe(11);
+        expect(students.length).toBe(11)
+        expect(students[5]).toBe("Montoya@visualpartnership.xyz")
     })
     test(') Test for getStudentsBycredits', () => {
         const students = StudentController.getStudentsByCredits(credits, path)
         
-        expect(students.length).toBe(15);
+        expect(students.length).toBe(15)
     })
 })
